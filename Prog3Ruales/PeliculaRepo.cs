7083282300a4b0c5e08b65ebb789a758b431f9ed
MathiasRuales <mathias.david.ruales@gmail.com>
2025-07-10 08:23:06 -0500

    Se añadio la logica para añadir una pelicula

--- Prog3Ruales/PeliculaRepo.cs
+++ Prog3Ruales/PeliculaRepo.cs
@@ -27,10 +27,28 @@ namespace Prog3Ruales
             _dbPath = dbPath;
         }
 
-        public async Task AddNewPelicula(String Titulo, String Genero, int AnioEstreno, int Calificacion )
+        public async Task AddNewPelicula(String Titulo, String Genero, int AnioEstreno, int Calificacion)
         {
-            
+            try
+            {
+                await Init();
+                if (AnioEstreno < 2023 && Calificacion < 3)
+                    throw new Exception("Año de estreno debe ser mayor o igual a 2023 y calificación debe ser mayor o igual a 3.");
 
+                int rowsAdded = await conn.InsertAsync(new Pelicula
+                {
+                    Titulo = Titulo,
+                    Genero = Genero,
+                    AnioEstreno = AnioEstreno,
+                    Calificacion = Calificacion
+                });
+                StatusMessage = $"{rowsAdded} fila(s) insertada(s).";
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error al insertar: {ex.Message}";
+
+            }
         }
                                                                                                                                           
         public async Task<List<Pelicula>> GetPeliculaAsync()
