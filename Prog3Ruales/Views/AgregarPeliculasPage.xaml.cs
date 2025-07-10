cc473543168426a2d2782aaabef2217cc8744215
MathiasRuales <mathias.david.ruales@gmail.com>
2025-07-10 09:01:53 -0500

    Headers

--- Prog3Ruales/Views/AgregarPeliculasPage.xaml.cs
+++ Prog3Ruales/Views/AgregarPeliculasPage.xaml.cs
@@ -1,9 +1,19 @@
+using Prog3Ruales;
+using System.Collections.Generic;
+using Prog3Ruales.Models;
 namespace Prog3Ruales.Views;
 
 public partial class AgregarPeliculasPage : ContentPage
 {
 	public AgregarPeliculasPage()
 	{
+
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		
+
+    }
 }
